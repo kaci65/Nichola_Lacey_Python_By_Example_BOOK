07edeffcd333d84651fabcf0f342dbfd65c03a33
kaci65 <wanjikukarugi@gmail.com>
2021-05-02 14:07:33 +0300

    jjj

---
+++ Reading_and_Writing_to_a_Text_File/names_txt.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Create a new file called 'Names.txt'
+
+file = open("Names.txt", "w")
+file.write("Luke\n")
+file.write("Jordan\n")
+file.write("Martha\n")
+file.write("Kerry\n")
+file.write("Jane\n")
+
+file.close()
